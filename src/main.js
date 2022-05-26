dced82a1a875477ed43f612c350f24958cc8c799
Vincent Vaxelaire <68623931+VincentVaxelaire@users.noreply.github.com>
2022-05-26 11:47:57 +0200

    init setup

--- src/main.js
+++ src/main.js
@@ -1,2 +1,20 @@
 import "./main.css";
 
+const sizes = {
+  WIDTH: window.innerWidth,
+  HEIGHT: window.innerHeight,
+}
+
+const canvas = document.querySelector("[data-canvas]");
+canvas.width = sizes.WIDTH * devicePixelRatio;
+canvas.height = sizes.HEIGHT * devicePixelRatio;
+canvas.style.width = `${sizes.WIDTH}px`;
+canvas.style.height = `${sizes.HEIGHT}px`;
+const ctx = canvas.getContext("2d");
+
+const frame = (ts) => {
+  ts /= 1000;
+
+
+  requestAnimationFrame(frame);
+}; frame()
