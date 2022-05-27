abd2df6f4c3394572a62117d4cee2b6f47218124
Vincent Vaxelaire <68623931+VincentVaxelaire@users.noreply.github.com>
2022-05-27 12:20:25 +0200

    circular movement

--- src/main.js
+++ src/main.js
@@ -19,15 +19,26 @@ const frame = (ts) => {
 
   const circleX = sizes.WIDTH / 2;
   const circleY = sizes.HEIGHT / 2;
-  // heart beat animation
-  const circleRadius = 100 + Math.cos(Math.abs(ts)*5)*2;
+
+  const circleRadius = 200;
   const circleStartAngle = 0;
   const circleEndAngle = Math.PI * 2;
   const circleCounterClockwise = false;
 
-  ctx.fillStyle = "blue";
+  ctx.strokeStyle = "black";
   ctx.beginPath();
   ctx.arc( circleX, circleY, circleRadius, circleStartAngle, circleEndAngle, circleCounterClockwise );
+  ctx.stroke();
+
+
+  ctx.fillStyle = "red";
+  const angle = ts * 2;
+
+  const moveX = circleX + circleRadius * Math.cos(angle);
+  const moveY = circleY + circleRadius * Math.sin(angle);
+
+  ctx.beginPath();
+  ctx.arc( moveX, moveY, 20, circleStartAngle, circleEndAngle, circleCounterClockwise );
   ctx.fill();
 
   requestAnimationFrame(frame);
