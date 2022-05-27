6a29fa5fec0dbe91ecc200d9c40d10c16125a650
Vincent Vaxelaire <68623931+VincentVaxelaire@users.noreply.github.com>
2022-05-27 14:16:02 +0200

    rotate object arround a point
    with nor without mouse position

--- src/main.js
+++ src/main.js
@@ -1,37 +1,68 @@
 import "./main.css";
 
+const canvas = document.querySelector("[data-canvas]");
+const ctx = canvas.getContext("2d");
+
 const sizes = {
   WIDTH: window.innerWidth,
   HEIGHT: window.innerHeight,
 }
 
-const canvas = document.querySelector("[data-canvas]");
+// determine mouse position
+const mousePosition = {
+  x: 0,
+  y: 0,
+}
+canvas.addEventListener("mousemove", (event) => {
+  const mouseX = event.layerX;
+  const mouseY = event.layerY;
+  mousePosition.x = mouseX;
+  mousePosition.y = mouseY;
+})
+
 canvas.width = sizes.WIDTH * devicePixelRatio;
 canvas.height = sizes.HEIGHT * devicePixelRatio;
 canvas.style.width = `${sizes.WIDTH}px`;
 canvas.style.height = `${sizes.HEIGHT}px`;
-const ctx = canvas.getContext("2d");
-
-let angle1 = 0;
-let angle2 = 0;
 
 const frame = (ts) => {
   ts /= 1000;
 
   ctx.clearRect(0, 0, sizes.WIDTH, sizes.HEIGHT);
 
-  const circleX = sizes.WIDTH / 2;
-  const circleY = sizes.HEIGHT / 2;
-  const circleRadius = 100;
+  const centerX = sizes.WIDTH / 2;
+  const centerY = sizes.HEIGHT / 2;
+  const arrowWidth = 100;
+  
+  ctx.fillStyle = "red";
+  ctx.strokeStyle = "blue";
+  ctx.lineWidth = 2;
 
-  ctx.fillStyle = "black";
-  ctx.beginPath();
-  // create wave movement
-  ctx.arc(circleX + Math.sin(angle1) * 1000, circleY + Math.cos(angle2) * 50, circleRadius, 0, Math.PI * 2);
-  ctx.fill();
+  const dx = mousePosition.x - centerX;
+  const dy = mousePosition.y - centerY;
+  const angle = Math.atan2(dy, dx);
 
-  angle1 += 0.01;
-  angle2 += 0.04;
+  ctx.save();
+    ctx.translate(centerX, centerY);
+
+    // control animation with mouse
+    ctx.rotate(angle);
+    // // linear animation
+    // ctx.rotate(ts);  
+
+    ctx.beginPath();
+    ctx.moveTo(0, 0);
+    ctx.lineTo(0, -10);
+    ctx.lineTo(0 + arrowWidth / 2, -10);
+    ctx.lineTo(0 + arrowWidth / 2, -20);
+    ctx.lineTo(0 + arrowWidth, 0);
+    ctx.lineTo(0 + arrowWidth / 2, 20);
+    ctx.lineTo(0 + arrowWidth / 2, 10);
+    ctx.lineTo(0, 10);
+    ctx.closePath();
+    ctx.fill();
+    ctx.stroke();
+  ctx.restore();
 
   requestAnimationFrame(frame);
 }; frame()
