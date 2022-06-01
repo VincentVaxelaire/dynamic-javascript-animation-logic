84ee1a28ab1a4df6d3bef7a394bce328e205d0a1
Vincent Vaxelaire <68623931+VincentVaxelaire@users.noreply.github.com>
2022-06-01 15:51:38 +0200

    simple velocity on two awes

--- src/main.js
+++ src/main.js
@@ -8,61 +8,30 @@ const sizes = {
   HEIGHT: window.innerHeight,
 }
 
-// determine mouse position
-const mousePosition = {
-  x: 0,
-  y: 0,
-}
-canvas.addEventListener("mousemove", (event) => {
-  const mouseX = event.layerX;
-  const mouseY = event.layerY;
-  mousePosition.x = mouseX;
-  mousePosition.y = mouseY;
-})
-
 canvas.width = sizes.WIDTH * devicePixelRatio;
 canvas.height = sizes.HEIGHT * devicePixelRatio;
 canvas.style.width = `${sizes.WIDTH}px`;
 canvas.style.height = `${sizes.HEIGHT}px`;
 
+let posX = 0
+let posY = 0
+
+let oldTime = 0
+
+// const velocityX = Math.random() * 2 - 1;
+// const velocityY = Math.random() * 2 - 1;
+
 const frame = (ts) => {
-  ts /= 1000;
+  ts /= 1000
+  const dt = ts - oldTime
+  oldTime = ts
 
-  ctx.clearRect(0, 0, sizes.WIDTH, sizes.HEIGHT);
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
 
-  const centerX = sizes.WIDTH / 2;
-  const centerY = sizes.HEIGHT / 2;
-  const arrowWidth = 100;
-  
-  ctx.fillStyle = "red";
-  ctx.strokeStyle = "blue";
-  ctx.lineWidth = 2;
+  posX += dt * 100
+  posY += dt * 50
 
-  const dx = mousePosition.x - centerX;
-  const dy = mousePosition.y - centerY;
-  const angle = Math.atan2(dy, dx);
-
-  ctx.save();
-    ctx.translate(centerX, centerY);
-
-    // control animation with mouse
-    ctx.rotate(angle);
-    // // linear animation
-    // ctx.rotate(ts);  
-
-    ctx.beginPath();
-    ctx.moveTo(0, 0);
-    ctx.lineTo(0, -10);
-    ctx.lineTo(0 + arrowWidth / 2, -10);
-    ctx.lineTo(0 + arrowWidth / 2, -20);
-    ctx.lineTo(0 + arrowWidth, 0);
-    ctx.lineTo(0 + arrowWidth / 2, 20);
-    ctx.lineTo(0 + arrowWidth / 2, 10);
-    ctx.lineTo(0, 10);
-    ctx.closePath();
-    ctx.fill();
-    ctx.stroke();
-  ctx.restore();
+  ctx.fillRect(posX, posY, 40, 40)
 
   requestAnimationFrame(frame);
 }; frame()
